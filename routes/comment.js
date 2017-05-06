06d24ec13bc8417d68a2d4123179385ded5fbca5
Jay <wenj5@student.unimelb.edu.au>
2017-05-06 21:55:18 +1000

    access_Token

--- routes/comment.js
+++ routes/comment.js
@@ -10,6 +10,7 @@ var db = new neo4j(
 );
 var parent_node_id = 21;
 var parent_question = "This is a question";
+var access_token = "";
 var Schema = mongoose.Schema
 var Item = new Schema({
   user_id: {
@@ -30,6 +31,12 @@ var Item = new Schema({
   node_id : {
     type : String,
     default:''
+  },
+  color:{
+    type: Number
+  },
+  count:{
+    type: Number
   },
   created:{
     type:Date,
@@ -71,7 +78,7 @@ router.get('/', function(req, res, next) {
 
 router.get('/test1', function (req, res, next) {
     request(
-        'https://graph.facebook.com/v2.9/515904358769189/comments?limits=100&access_token=EAACEdEose0cBAOfD8Hx6SlfMQZCWAFxoS2LIKg17LcAeejGEgODRsPy0KGlkg9AXcYDCbsKMrs28QRlQUBdF9NQsYiNHhLuTGkvh7h5Pf1dQdfWFe6H4R4eUVEwijpuJzFGDDkDqUqtt8iC1PNbLnmzExEey5qDHZCgGpiAibaYV5Gm98u8bFp9UmPWsAZD',
+        'https://graph.facebook.com/v2.9/515904358769189/comments?limits=100&access_token='+access_token,
         function (error, response, body) {
             if(error){
               return res.send("error");
@@ -82,13 +89,6 @@ router.get('/test1', function (req, res, next) {
             if(json == null || json.data ==null ||json.data.length == 0){
               return res.send("no data");
             }
-            // for(i=0;i<json.data.length;i++) {
-            //     var createTime = json.data[i].created_time;
-            //     var message = json.data[i].message;
-
-            //     // console.log(createTime);
-            //     // console.log(message);
-            // }
 
             async.eachSeries(json.data, function(obj, callback){
                var createTime = obj.created_time;
@@ -280,6 +280,7 @@ router.get('/test3', function(req, res, next){
   });
 })
 
+//formal function
 
 router.get("/cleardb",function(req, res, next){
   //
@@ -301,18 +302,175 @@ router.get("/cleardb",function(req, res, next){
 });
 
 
+router.get('/test1', function (req, res, next) {
+    request(
+        'https://graph.facebook.com/v2.9/515904358769189/comments?limits=100&access_token='+access_token,
+        function (error, response, body) {
+            if(error){
+              return res.send("error");
+            }
+            var json = JSON.parse(body);
+            console.log(json.data);
+            var question_id = req.query.q_id  == null ? parent_node_id : req.query.q_id;
+            if(json == null || json.data ==null ||json.data.length == 0){
+              return res.send("no data");
+            }
 
-// db.insertNode({
-//   name: 'Ghuffran',
-//   company: 'Modulus',
-//   age: ~~(Math.random() * 100)
-// }, function (err, node) {
-//   if (err) {
-//     return console.log(err);
-//   }
+            async.eachSeries(json.data, function(obj, callback){
+               var createTime = obj.created_time;
+                var message = obj.message;
+                db.insertNode({
+                }, function (err, node) {
+                  var id = node._id;
+                  if (err) {
+                    return callback(err)
+                  }else{
+                        var item = {
+                          user_id : '',
+                          question_id : question_id,
+                          node_id : id,
+                          type : false,
+                          message : message,
+                          created : createTime
+                        }
 
-//   // Output node data.
-//   console.log(node);
-// });
+                        ItemModal.create(item, (err, docs) => {
+                            if (err) {
+                              callback(err);
+                            } else {
+                              var sql_child = "MATCH (s),(m) WHERE ID(s) = "+question_id+" AND ID(m) = "+id+" CREATE (s)-[r:Answer]->(m) return s,m,r"
+                              // var sql_child = "MATCH (a { ID: '"++"' }), (b { ID: '"+id+"' }) CREATE (a)-[:Answer]->(b)";
+                              console.log(sql_child);
+                                  db.cypherQuery(sql_child, function (err, result) {
+                                        if (err) {
+                                               callback(err);
+                                        }else{
+                                            // res.send(result);
+                                             callback(null);
+                                        }
+                                        // json.name = result.data.name
+                                        // console.log(result)
+
+                                  })
+                            }
+                          })
+                  }
+                });
+            },function done(err) {
+                if(err){
+                  res.send(err);
+                }else{
+                  res.send({});
+                }
+            });
+            // console.log('error:', error) // Print the error if one occurred
+            // console.log('statusCode:', response && response.statusCode) // Print the response status code if a response was received
+            // // console.log('body:', body) // Print the HTML for the Google homepage.
+            // res.send(body);
+        }
+    )
+});
+
+
+
+
+
+router.get('/getdata', function(req, res, next){
+    var question_id = req.query.q_id  == null ? parent_node_id : req.query.q_id;
+    var sql_root = 'MATCH (s) WHERE ID(s) = '+question_id+' MATCH (s)-[:Answer]->(m) return m';
+    console.log(sql_root);
+    // var sql_child = 'Match(n:Children) return n';
+   
+    db.cypherQuery(sql_root, function (err, result) {
+        if (err) {
+            res.status(404).send(result);
+        }else{
+            // res.send(result);
+            // res.json(result);
+            if(result != null){
+              var json = result;//JSON.parse(result);
+              var data = json.data;
+              if(json == null || data == null){
+                res.status(404).send({});
+              }
+              var arr = [];
+              for(var i = 0 ; i < data.length; i++){
+                arr.push(data[i]["_id"]);
+              }
+              ItemModal.find({"node_id":{"$in":arr}}, function(err, result){
+                if(err){
+                  res.status(404).send({});
+                }else{
+                 
+                  var result_data = {};
+                  var node = new Array();
+                  var relationship = new Array();
+                  //parent_node
+                  /*
+                  */
+                  var id = parent_node_id;
+                  var name = parent_question;
+                  var color = new Array();
+                  color.push(_.random(0,10));
+                  color.push(_.random(0,10));
+                  color.push(_.random(0,10));
+                  color.push(_.random(0,10));
+                  color.push(_.random(0,10));
+                  color.push(_.random(0,10));
+                  //
+                  var size  = _.random(20,40);
+                  var tmp = {
+                       id : id,
+                       name: name,
+                       color : color,
+                       size: size
+
+                     }
+                     node.push(tmp);
+                  /*
+                  */
+                  for(var i = 0; i < result.length; i++){
+                    var id = result[i]["node_id"];
+                    var name = result[i]["message"];
+                    var color = new Array();
+                    color.push(_.random(0,10));
+                    color.push(_.random(0,10));
+                    color.push(_.random(0,10));
+                    color.push(_.random(0,10));
+                    color.push(_.random(0,10));
+                    color.push(_.random(0,10));
+                    var size  = _.random(20,40);
+                     var tmp = {
+                       id : id,
+                       name: name,
+                       color : color,
+                       size: size
+
+                     }
+
+                     var relation = {
+                       id_1: question_id,
+                       id_2: id
+                     }
+
+                     node.push(tmp);
+                     relationship.push(relation);
+                  }
+                  result_data["nodes"] = node;
+                  result_data["relationship"] = relationship;
+                 res.status(200).send(result_data);
+                }
+              })
+              
+            }else{
+              res.status(200).send({});
+            }
+        }
+        // json.name = result.data.name
+        // console.log(result)
+
+  })
+})
+
 
 module.exports = router;
